590813613453c633fc7d1c9669b513822b2e6521
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2018-02-26 17:05:48 +0300

    master. Added license information. Build to release version. Build Under ubuntu.

--- README.md
+++ README.md
@@ -11,4 +11,28 @@ Usage: fingerprint_for_wilton [-h] [-f|--file=STRING] [-t|--timeout=INT] [-h|--h
 ```
 
 ## warning
-Uses 32-bit library libScanAPI.
+1. Uses 32-bit library libScanAPI.
+
+2. To use without "sudo" You should add special usb rule. By default FD80 connected in "root" goup under ubuntu, not "dialout".
+Add file with rules to "/etc/udev/rules.d", for example:
+"42-fingerprint-futronic.rules"
+```
+ATTR(manufacturer)=="Futronic Technology Company Ltd.", GROUP="dialout"
+```
+
+At real machine looks like:
+```
+/etc/udev/rules.d$ cat 42-fingerprint-futronic.rules
+ATTR{manufacturer}=="Futronic Technology Company Ltd.", GROUP="dialout"
+```
+
+Now You may add user to dialout group to use fingerprint without sudo rights:
+```
+  sudo usermod -a -G dialout userName
+```
+
+of couse you can use group other than dialout.
+
+### Work with reader based on:
+https://github.com/erikssm/futronics-fingerprint-reader
+
--- src/ftrScan.cpp
+++ src/ftrScan.cpp
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2018, mike at github.com/iskinmike
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * File:   ftrScan.cpp
+ * Author: mike
+ *
+ * Created on February 21, 2018, 4:39 PM
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -95,7 +118,7 @@ int ftr_get_scan(unsigned char **out_buffer, int& out_width, int& out_height, ui
     int count = 0;
     while(1)
     {
-      ftrScanSetDiodesStatus(hDevice, (unsigned int)100/2, (unsigned int)100/2); // green led ON, red led ON
+      ftrScanSetDiodesStatus(hDevice, (unsigned int)100/2, (unsigned int)100/2);
       if( ftrScanIsFingerPresent( hDevice, NULL ) )
         break;
       usleep(sleep_microseconds);
@@ -135,7 +158,7 @@ int ftr_get_scan(unsigned char **out_buffer, int& out_width, int& out_height, ui
 }
 
 void ftr_close_device() {
-  ftrScanSetDiodesStatus(hDevice, 0, 0); // green led OFF, red led OFF
+  ftrScanSetDiodesStatus(hDevice, 0, 0);
   free( pBuffer );
   ftrScanCloseDevice( hDevice );
 }
